66990d4dbe4aa61701b976b4c8e17c4102a9bbb9
nicholasjhs <n.hughsam@gmail.com>
2019-09-23 12:21:45 -0400

    Added Bulma to project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CheckOut/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CheckOut from './components/CheckOut.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CheckOut,
   }
 }
 </script>
---
+++ src/components/CheckOut.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="checkout-container">
+      <div class="checkout-header">
+
+      </div>
+      <div class="checkout-body">
+          <div class="checkout-column checkout-product">
+
+          </div>
+          <div class="checkout-column checkout-details">
+              
+          </div>
+      </div>
+      <div class="checkout-footer">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckOut',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
