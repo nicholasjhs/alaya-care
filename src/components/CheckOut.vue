3b12a4d53098e99f7ccbdf57f8c98c797f11a0e8
nicholasjhs <n.hughsam@gmail.com>
2019-09-23 22:13:36 -0400

    Finished the basic styling, missing validations and responsiveness

--- src/components/CheckOut.vue
+++ src/components/CheckOut.vue
@@ -10,46 +10,60 @@
                       <a href="#">Shipping Info</a>
                   </li>
                   <li class="nav-item">
-                      <a href="#">Payment</a>
+                      <a class="active" href="#">Payment</a>
                   </li>
               </ul>
           </nav>
       </div>
       <div class="checkout-body row">
           <div class="column checkout-product">
-              <img class="product-image" src="https://via.placeholder.com/150">
-              <h5 class="">13-inch MacBook Pro - Space Grey</h5>
-              <p class="">To pay</p>
-              <h2 class="">1,799.00 <span>USD</span></h2>
-              <ul class="product-details">
-                  <li>Intel Core i5 2.3GHz</li>
-                  <li>8GB memory</li>
-                  <li>256GB SSD storage</li>
-              </ul>
+              <div class="product-header">
+                <img class="product-image" src="https://via.placeholder.com/150">
+                <h3 class="product-title">13-inch MacBook Pro - Space Grey</h3>
+              </div>
+              <div class="product-body">
+                <p class="to-pay">To pay</p>
+                <h2 class="product-price">1,799.00 <span>USD</span></h2>
+                <ul class="product-details">
+                    <li class="product-item">Intel Core i5 2.3GHz</li>
+                    <li class="product-item">8GB memory</li>
+                    <li class="product-item">256GB SSD storage</li>
+                </ul>
+              </div>
 
           </div>
-          <div class="column checkout-details">
-              <h2 class="">Payment Details</h2>
-              <h5 class="">Fill in the info below to continue</h5>
-              <form>
-                  <div class="cc-number">
-                      <label for="ccnums">Creditcard number</label>
-                      <input type="text" id="ccnums" name="ccnums" placeholder="0000 0000 0000 0000">
-                  </div>
-                  <div class="cc-name">
-                      <label for="ccname">Cardholer name</label>
-                      <input type="text" id="ccname" name="ccname" placeholder="">
-                  </div>
-                  <div class="cc-expiry">
-                    <label for="ccmonth">Expiry month</label>
-                    <input type="text" id="ccmonth" name="ccmonth" placeholder="">
-                    <label for="ccyear">Expiry year</label>
-                    <input type="text" id="ccyear" name="ccyear" placeholder="">
-                    <label for="cvv">CVV</label>
-                    <input type="text" id="cvv" name="cvv" placeholder="">
-                  </div>
-                  <button>Checkout</button>
-              </form>
+          <div class="column checkout-payment">
+              <div class="payment-header">
+                <h1 class="payment-title">Payment details</h1>
+                <h3 class="payment-subtitle">Fill in the info below to continue</h3>
+              </div>
+              <div class="payment-body">
+                <form class="form">
+                    <div class="cc-number form-group">
+                        <label class="form-label" for="ccnums">Creditcard number</label>
+                        <input class="form-input" type="text" id="ccnums" name="ccnums" placeholder="4242 4242 4242 4242">
+                    </div>
+                    <div class="cc-name form-group">
+                        <label class="form-label" for="ccname">Cardholder name</label>
+                        <input class="form-input" type="text" id="ccname" name="ccname" placeholder="">
+                    </div>
+                    <div class="cc-expiry">
+                        <div class="form-group">
+                            <label class="form-label" for="ccmonth">Expiry month</label>
+                            <input class="form-input" type="text" id="ccmonth" name="ccmonth" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="ccyear">Expiry year</label>
+                            <input class="form-input" type="text" id="ccyear" name="ccyear" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="cvv">CVV</label>
+                            <input class="form-input" type="text" id="cvv" name="cvv" placeholder="">
+                        </div>
+                    </div>
+                    <button class="form-btn">Checkout</button>
+                </form>
+              </div>
           </div>
       </div>
       <div class="checkout-footer">
