603dd7b6468b5eec13e417276336006164f15b28
nicholasjhs <n.hughsam@gmail.com>
2019-09-24 18:46:14 -0400

    Refactored my code, missing the form validations

--- src/components/CheckOut.vue
+++ src/components/CheckOut.vue
@@ -1,65 +1,14 @@
 <template>
   <div class="checkout-container">
       <div class="checkout-header">
-          <nav class="checkout-nav">
-              <ul class="nav-links">
-                  <li class="nav-item">
-                      <a href="#">Customer Info</a>
-                  </li>
-                  <li class="nav-item">
-                      <a href="#">Shipping Info</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="active" href="#">Payment</a>
-                  </li>
-              </ul>
-          </nav>
+          <Nav :links="links"/>
       </div>
       <div class="checkout-body row">
           <div class="column checkout-product">
-              <div class="product-header">
-                <img class="product-image" src="https://via.placeholder.com/150">
-                <h3 class="product-title">13-inch MacBook Pro - Space Grey</h3>
-              </div>
-              <div class="product-body">
-                <p class="to-pay">To pay</p>
-                <h2 class="product-price">1,799.00 <span>USD</span></h2>
-                <ul class="product-details">
-                    <li class="product-item">Intel Core i5 2.3GHz</li>
-                    <li class="product-item">8GB memory</li>
-                    <li class="product-item">256GB SSD storage</li>
-                </ul>
-              </div>
-
+              <product :imageLink="product.image" :productName="product.name" :productPrice="product.price" :productDetails="product.details"></product>
           </div>
           <div class="column checkout-payment">
-              <div class="payment-header">
-                <h1 class="payment-title">Payment details</h1>
-                <h3 class="payment-subtitle">Fill in the info below to continue</h3>
-              </div>
-              <div class="payment-body">
-                <form class="form">
-                    <div class="cc-number form-group">
-                        <label class="form-label" for="ccnums">Creditcard number</label>
-                        <input class="form-input" type="text" id="ccnums" name="ccnums" placeholder="4242 4242 4242 4242">
-                    </div>
-                    <div class="cc-name form-group">
-                        <label class="form-label" for="ccname">Cardholder name</label>
-                        <input class="form-input" type="text" id="ccname" name="ccname" placeholder="">
-                    </div>
-                    <div class="cc-expiry">
-                        <div class="form-group">
-                            <label class="form-label" for="ccexpiry">Expiry date (MM/YY)</label>
-                            <input class="form-input" type="text" id="ccexpiry" name="ccexpiry" placeholder="">
-                        </div>
-                        <div class="form-group">
-                            <label class="form-label" for="cvv">CVV</label>
-                            <input class="form-input" type="text" id="cvv" name="cvv" placeholder="">
-                        </div>
-                    </div>
-                    <button class="form-btn">Checkout</button>
-                </form>
-              </div>
+              <Payment />
           </div>
       </div>
       <div class="checkout-footer">
@@ -69,8 +18,28 @@
 </template>
 
 <script>
+import Nav from './Nav.vue';
+import Product from './Product.vue';
+import Payment from './Payment.vue';
+
 export default {
   name: 'CheckOut',
+  components: {
+      Nav,
+      Product,
+      Payment
+  },
+  data() {
+      return {
+          product: {
+              image: "https://via.placeholder.com/150",
+              name: "13-inch MacBook Pro - Space Grey",
+              price: "1,799.00",
+              details: ["Intel Core i5 2.3GHz", "8GB memory", "256GB SSD storage"]
+          },
+          links: [{name: 'Customer info', active: false}, {name: 'Shipping info', active: false}, {name: 'Payment', active: true}],
+      }
+  },
   props: {
   }
 }
---
+++ src/components/Payment.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="payment-container">
+        <div class="payment-header">
+        <h1 class="payment-title">Payment details</h1>
+        <h3 class="payment-subtitle">Fill in the info below to continue</h3>
+        </div>
+        <div class="payment-body">
+        <form class="form">
+            <div class="cc-number form-group">
+                <label class="form-label" for="ccnums">Creditcard number</label>
+                <input class="form-input" type="text" id="ccnums" name="ccnums" placeholder="4242 4242 4242 4242">
+            </div>
+            <div class="cc-name form-group">
+                <label class="form-label" for="ccname">Cardholder name</label>
+                <input class="form-input" type="text" id="ccname" name="ccname" placeholder="">
+            </div>
+            <div class="cc-expiry">
+                <div class="form-group">
+                    <label class="form-label" for="ccexpiry">Expiry date (MM/YY)</label>
+                    <input class="form-input" type="text" id="ccexpiry" name="ccexpiry" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label class="form-label" for="cvv">CVV</label>
+                    <input class="form-input" type="text" id="cvv" name="cvv" placeholder="">
+                </div>
+            </div>
+            <button class="form-btn">Checkout</button>
+        </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Payment',
+  components: {
+
+  },
+  data() {
+      return {
+      }
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
